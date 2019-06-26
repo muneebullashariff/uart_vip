cd9f5910ff860688f4ca854e3e7a334aa7272499
chetanmelagiri <chetan.melagiri@gmail.com>
2019-06-26 11:03:46 +0530

    Added new files

---
+++ tb_files/src/master_agent/master_driver.sv
@@ -0,0 +1,107 @@
+//  ################################################################################################
+//
+//  Licensed to the Apache Software Foundation (ASF) under one or more contributor license 
+//  agreements. See the NOTICE file distributed with this work for additional information
+//  regarding copyright ownership. The ASF licenses this file to you under the Apache License,
+//  Version 2.0 (the"License"); you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software distributed under the 
+//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+//  either express or implied. See the License for the specific language governing permissions and 
+//  limitations under the License.
+//
+//  ################################################################################################
+
+//  2) Use of Include Guards
+`ifndef _master_driver.sv_INCLUDED_
+`define _master_driver.sv_INCLUDED_
+
+//------------------------------------------------------------------------------------------------//
+//  Class: master_driver
+//  A driver is written by extending the uvm_driver.uvm_driver is inherited from uvm_component, 
+//  Methods and TLM port (seq_item_port) are defined for communication between sequencer and driver.
+//  The uvm_driver is a parameterized class and it is parameterized with the type of the request
+//  sequence_item and the type of the response sequence_item. 
+//------------------------------------------------------------------------------------------------//
+class master_driver extends uvm_driver #(master_xtn);
+
+//------------------------------------------------------------------------------------------------//
+//  Factory registration is done by passing class name as argument.
+//  Factory Method in UVM enables us to register a class, object and variables inside the factory 
+//  so that we can override their type (if needed) from the test bench without needing to make any
+//  significant change in component structure.
+//------------------------------------------------------------------------------------------------//
+		`uvm_component_utils(master_driver)
+
+//------------------------------------------------------------------------------------------------//
+//  Virtual interface holds the pointer to the Interface.    
+//------------------------------------------------------------------------------------------------//
+	//	 virtual uart_if vif;
+		 master_agent_config w_cfg;
+//------------------------------------------------------------------------------------------------//
+//  The extern qualifier indicates that the body of the method (its implementation) is to be found 
+//  outside the declaration.
+//------------------------------------------------------------------------------------------------//
+		 extern function new (string name="master_driver", uvm_component parent);
+		 extern function void build_phase(uvm_phase phase);
+		 extern function void connect_phase(uvm_phase phase);
+		 extern task run_phase(uvm_phase phase);
+		 extern task send_to_dut(master_xtn xtn);
+
+endclass:master_driver
+
+//----------------------------------------New_Constructor-----------------------------------------//
+//  The new function is called as class constructor. On calling the new method it allocates the 
+//  memory and returns the address to the class handle. For the component class two arguments to be 
+//  passed. 
+//------------------------------------------------------------------------------------------------//
+	function master_driver::new(string name = "master_driver", uvm_component parent);
+		super.new(name, parent);
+	endfunction:new
+
+//----------------------------------------Build_Phase---------------------------------------------//
+//  The build phases are executed at the start of the UVM Testbench simulation and their overall 
+//  purpose is to construct, configure and connect the Testbench component hierarchy.
+//  All the build phase methods are functions and therefore execute in zero simulation time.
+//------------------------------------------------------------------------------------------------//
+	function void master_driver::build_phase(uvm_phase phase);
+		super.build_phase(phase);
+		if(!uvm_config_db #(master_agent_config)::get(this,"","master_agent_config",w_cfg))
+			`uvm_fatal("CONFIG","Cannot get() w_cfg from uvm_config_db. Have you set() it?")
+	endfunction:build_phase
+
+//----------------------------------------Connect_Phase-------------------------------------------//
+//  The connect phase is used to make TLM connections between components or to assign handles to 
+//  testbench resources. It has to occur after the build method so that Testbench component 
+//  hierarchy could be in place and it works from the bottom-up of the hierarchy upwards.
+//------------------------------------------------------------------------------------------------//
+	function void master_driver::connect_phase(uvm_phase phase);
+	//	vif = w_cfg.vif;
+	endfunction:connect_phase
+
+
+//----------------------------------------Run_Phase-----------------------------------------------//
+//  The run phase is used for the stimulus generation and checking activities of the Testbench. 
+//  The run phase is implemented as a task, and all uvm_component run tasks are executed in parallel.
+//------------------------------------------------------------------------------------------------//
+	task master_driver::run_phase(uvm_phase phase);
+				forever
+				  begin
+						seq_item_port.get_next_item(req);
+							send_to_dut(req);
+						seq_item_port.item_done();
+					end 
+	endtask:run_phase
+
+//------------------------------------------------------------------------------------------------//
+//  Task:send_to_dut
+//  Task send_to_dut is used to write the driving logic of the driver.
+//------------------------------------------------------------------------------------------------//
+	task master_driver::send_to_dut(master_xtn xtn);
+
+//  Logic to be updated
+	 			
+	endtask:send_to_dut
---
+++ tb_files/src/master_agent/master_xtn.sv
@@ -0,0 +1,54 @@
+//  ###########################################################################
+//
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the
+//  "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
+//
+//  ###########################################################################
+//   Use of Include Guards
+`ifndef _master_xtn.sv_INCLUDED_
+`define _master_xtn.sv_INCLUDED_
+
+class master_xtn extends uvm_sequence_item;
+
+//Factory registration is done by passing class name as argument.
+//Factory Method in UVM enables us to register a class, object and variables inside the factory 
+//so that we can override their type (if needed) from the test bench without needing to make any
+//significant change in component structure.
+  `uvm_object_utils(master_xtn)
+	
+		extern function new(string name = "master_xtn");
+		extern function void do_print(uvm_printer printer);
+	endclass:master_xtn
+
+//----------------------------------------New_Constructor-----------------------------------------//
+  //The new function is called as class constructor. On calling the new method it allocates the 
+  //memory and returns the address to the class handle. For the component class two arguments to be 
+  //passed. 
+	function master_xtn::new(string name="master_xtn");
+		super.new(name);
+	endfunction:new
+
+
+//----------------------------------------Do_Print-----------------------------------------//
+	function void  master_xtn::do_print (uvm_printer printer);
+    super.do_print(printer);
+       //                   srting name   		bitstream value     size       radix for printing 
+  endfunction:do_print
+    
+
+  
+
