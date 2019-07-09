d41a3d77547976de79ac77b4b51602fc086086c3
Mounica.G <48309846+mounicagundluru@users.noreply.github.com>
2019-07-09 14:26:45 +0530

    Update base_test.sv

--- tb_files/src/test/base_test.sv
+++ tb_files/src/test/base_test.sv
@@ -34,14 +34,14 @@ class base_test extends uvm_test;
 
 //Declaring handles for different components
     	 tb envh;
-       env_config e_cfg;
-			 master_agent_config w_cfg[];
-			 slave_agent_config r_cfg[];
+       	 env_config e_cfg;
+	 master_agent_config w_cfg[];
+	 slave_agent_config r_cfg[];
 
-       int no_of_ragent = 1;
- 			 int no_of_wagent=1;
-       int has_ragent = 1;
-       int has_wagent = 1;
+         int no_of_ragent = 1;
+ 	 int no_of_wagent=1;
+         int has_ragent = 1;
+         int has_wagent = 1;
 
   //---------------------------------------------
   // Externally defined tasks and functions
@@ -49,7 +49,7 @@ class base_test extends uvm_test;
 	extern function new(string name = "base_test" , uvm_component parent);
 	extern function void configuration();
 	extern function void build_phase(uvm_phase phase);
-  extern task run_phase(uvm_phase phase);
+  	extern task run_phase(uvm_phase phase);
 endclass:base_test
 
 
@@ -61,55 +61,49 @@ endclass:base_test
 //  name - instance name of the config_template
 //  parent - parent under which this component is created
 //-----------------------------------------------------------------------------
-	function base_test::new(string name = "base_test" , uvm_component parent);
-		super.new(name,parent);
-	endfunction:new
+function base_test::new(string name = "base_test" , uvm_component parent);
+	super.new(name,parent);
+endfunction:new
 
 
 //------------------------------------------------------------------------------------------------
 //function:configuration()
 //This gives information about creating a component abd regestring the id to factory
 //--------------------------------------------------------------------------------------------------
-	function void base_test::configuration();
- 	  	   e_cfg.w_cfg=new[no_of_wagent];
-        	e_cfg.r_cfg=new[no_of_ragent];
+function void base_test::configuration();
+ 	 e_cfg.w_cfg=new[no_of_wagent];
+         e_cfg.r_cfg=new[no_of_ragent];
 
-
-		if (has_wagent) 
-				 begin
-            w_cfg = new[no_of_wagent];
+	 if (has_wagent) 
+	 begin
+         w_cfg = new[no_of_wagent];
+	 foreach(w_cfg[i]) 
+	 	begin
+         	w_cfg[i]=master_agent_config::type_id::create($sformatf("w_cfg[%0d]", i));
+	  	if(!uvm_config_db #(virtual uart_if)::get(this,"", $sformatf("vif_%0d",i),w_cfg[i].vif))
+	  	`uvm_fatal("VIF CONFIG","cannot get()interface vif from uvm_config_db. Have you set() it?") 
+	  	w_cfg[i].is_active = UVM_ACTIVE;
+          	e_cfg.w_cfg[i] = w_cfg[i];
+          	end
+          end
 	
-	        	foreach(w_cfg[i]) 
-							begin
-              	 w_cfg[i]=master_agent_config::type_id::create($sformatf("w_cfg[%0d]", i));
-							   if(!uvm_config_db #(virtual uart_if)::get(this,"", $sformatf("vif_%0d",i),w_cfg[i].vif))
-							  `uvm_fatal("VIF CONFIG","cannot get()interface vif from uvm_config_db. Have you set() it?") 
-              
-								w_cfg[i].is_active = UVM_ACTIVE;
-                e_cfg.w_cfg[i] = w_cfg[i];
-                
-                end
-            end
-            
-			if (has_ragent) 
-				 begin
-           r_cfg=new[no_of_ragent];
-
-						foreach(r_cfg[i]) 
-							begin
-           	    r_cfg[i]=slave_agent_config::type_id::create($sformatf("r_cfg[%0d]",i));
-								if(!uvm_config_db #(virtual uart_if)::get(this,"",$sformatf("vif_%0d",i),r_cfg[i].vif))
-									`uvm_fatal("VIF CONFIG","cannot get() interface vif from uvm_config_db. Have you set() it?")
-										r_cfg[i].is_active = UVM_ACTIVE;
-										e_cfg.r_cfg[i]=r_cfg[i];
-               
-              end
-				end	
-        e_cfg.no_of_ragent = no_of_ragent;
-        e_cfg.no_of_wagent = no_of_wagent;
+         if (has_ragent) 
+	 begin
+         r_cfg=new[no_of_ragent];
+	 foreach(r_cfg[i]) 
+	 	begin
+         	r_cfg[i]=slave_agent_config::type_id::create($sformatf("r_cfg[%0d]",i));
+	 	if(!uvm_config_db #(virtual uart_if)::get(this,"",$sformatf("vif_%0d",i),r_cfg[i].vif))
+	 	`uvm_fatal("VIF CONFIG","cannot get() interface vif from uvm_config_db. Have you set() it?")
+	 	r_cfg[i].is_active = UVM_ACTIVE;
+	 	e_cfg.r_cfg[i]=r_cfg[i]; 
+         	end
+	 end	
+         e_cfg.no_of_ragent = no_of_ragent;
+         e_cfg.no_of_wagent = no_of_wagent;
 									 
-        e_cfg.has_ragent = has_ragent;
-        e_cfg.has_wagent = has_wagent;
+         e_cfg.has_ragent = has_ragent;
+         e_cfg.has_wagent = has_wagent;
   endfunction: configuration
 
 
@@ -120,32 +114,30 @@ endclass:base_test
 // Parameters:
 //  phase - stores the current phase 
 //-----------------------------------------------------------------------------
-	function void base_test::build_phase(uvm_phase phase);
-		e_cfg=env_config::type_id::create("e_cfg");
+function void base_test::build_phase(uvm_phase phase);
+	e_cfg=env_config::type_id::create("e_cfg");
                
-		if(has_wagent)
-       e_cfg.w_cfg = new[no_of_wagent];
+	if(has_wagent)
+        e_cfg.w_cfg = new[no_of_wagent];
           
-		if(has_ragent)
-       e_cfg.r_cfg = new[no_of_ragent];
+	if(has_ragent)
+        e_cfg.r_cfg = new[no_of_ragent];
 
-    configuration();
-	 	uvm_config_db #(env_config)::set(this,"*","env_config",e_cfg);
-    super.build_phase(phase);
-		envh=tb::type_id::create("envh", this);
-	endfunction:build_phase
+    	configuration();
+	uvm_config_db #(env_config)::set(this,"*","env_config",e_cfg);
+        super.build_phase(phase);
+	envh=tb::type_id::create("envh", this);
+endfunction:build_phase
 
   //--------------------------------------------------------------------------------
   //Task:run_phase
   //--------------------------------------------------------------------------------
 
-  task base_test::run_phase(uvm_phase phase);
-
-    phase.raise_objection(this);
-    #10;
-    phase.drop_objection(this);
-
-  endtask
+task base_test::run_phase(uvm_phase phase);
+      phase.raise_objection(this);
+      #10;
+      phase.drop_objection(this);
+endtask
 
 
 
