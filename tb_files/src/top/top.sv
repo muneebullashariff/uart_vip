d198da8bc1f81bad7d0624d7b353dcc48ab30a09
Mounica.G <48309846+mounicagundluru@users.noreply.github.com>
2019-07-09 14:32:57 +0530

    Update top.sv

--- tb_files/src/top/top.sv
+++ tb_files/src/top/top.sv
@@ -25,38 +25,33 @@
 //-------------------------------------------------------------------------------------------------
 module top;
 	
-  import test_pkg::*;
-  import uvm_pkg::*;
-		
-  logic reset_n;
+	import test_pkg::*;
+  	import uvm_pkg::*;	
+  	logic reset_n;
 
-  initial
-  begin
-    // Asserting the active_low reset
-    reset_n = 0;
-    // De-asserting the active_low reset
-    #10 reset_n = ~reset_n;
-  end
+  	initial
+  	begin
+// Asserting the active_low reset
+  	reset_n = 0;
+// De-asserting the active_low reset
+    	#10 reset_n = ~reset_n;
+  	end
+// Instantiation of UART interface
+  	uart_if intf(.reset(reset_n));
 
-  // Instantiation of UART interface
-  uart_if intf(.reset(reset_n));
-
-  // TODO: Need to have the loopback mode
-  // assign intf.rx = intf.tx;
+// TODO: Need to have the loopback mode
+// assign intf.rx = intf.tx;
   
-  initial
-  begin
-    // TODO: Need to suppor for multiple interfaces
-    // Temporarily having vif_0
-    uvm_config_db #(virtual uart_if)::set(null,"*","vif_0",intf);
+  	initial
+  	begin
+// TODO: Need to suppor for multiple interfaces
+// Temporarily having vif_0
+        uvm_config_db #(virtual uart_if)::set(null,"*","vif_0",intf);
      
-    //running the basic test case
-    run_test("base_test");
-    //  $display("running base_test in top");
-  end
- 
-
-   
+//running the basic test case
+    	run_test("base_test");
+//  $display("running base_test in top");
+ 	 end  
 endmodule
 
 
